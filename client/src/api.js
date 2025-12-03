543b86aee59411fd3026185acb3587e18c56d7e5
Mikiale <mikialegetachew12@gmail.com>
2025-12-03 02:25:28 -0800

    connected frontend to render backend

---
+++ client/src/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_URL = import.meta.env.MODE === 'production' 
+  ? 'https://coursebete.onrender.com/api' 
+  : 'http://localhost:5000/api';
+
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
