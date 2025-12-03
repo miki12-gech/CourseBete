c80038ac6f53a9052fe1b3b198107285d52e2f95
Mikiale <mikialegetachew12@gmail.com>
2025-12-03 05:34:54 -0800

    fix cors issue

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(cors());
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json()); 
 
 
