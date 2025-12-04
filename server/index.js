8e1598de716fae5301292473effb99815b97c8e5
Mikiale <mikialegetachew12@gmail.com>
2025-12-04 02:09:43 -0800

    fix cors for vercel

--- server/index.js
+++ server/index.js
@@ -8,10 +8,15 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors({
-    origin: '*', 
+    origin: [
+        'http://localhost:5173', 
+        'https://senbet-tmhrt.vercel.app' // <--- REPLACE THIS with your exact Vercel Link!
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
+
 app.use(express.json()); 
 
 
